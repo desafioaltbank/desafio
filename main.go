606d27c90022cc98605cb0af3dfc9a73296c6743
desafioaltbank <desafio.alt.bank@gmail.com>
2024-01-30 17:04:52 -0300

    test env cd 4

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/altbankdesafio/handlers"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Welcome API MATH - Alt Bank Challenge</h1>"))
+		w.Write([]byte("<h1>Welcome API MATH - Alt Bank Challenge - </h1>"))
 	})
 
 	http.HandleFunc("/prime", handlers.PrimeHandler)
